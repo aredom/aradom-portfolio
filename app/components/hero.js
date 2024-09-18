612bb40327af39ece4153bf0486cb344335dc919
aredom <leakearedom23@gmail.com>
2024-09-18 16:15:14 +0300

    my comment

--- app/components/hero.js
+++ app/components/hero.js
@@ -23,27 +23,28 @@ export default function Hero() {
           <div className="md:w-1/2 text-center md:text-left">
             <h1 className="text-4xl md:text-5xl font-bold mb-4">Aradom Leake</h1>
             <div className="h-8 mb-4 text-blue-500 font-bold">
-              <TypedText strings=" Web Developer, Full Stack Developer, Web Designer, Apps Designer, Apps Developer" />
+              <TypedText strings="Web Developer, Full Stack Developer, Web Designer, Apps Designer, Apps Developer" />
             </div>
             <p className="text-xl mb-6">Turning ideas into reality through code and design.</p>
             
             <div className="flex justify-center md:justify-start space-x-4 mb-8">
               <Link href="#">
-              <FontAwesomeIcon icon={faTwitter} className="text-gray-300 hover:text-white w-6 h-6" />
+                <FontAwesomeIcon icon={faTwitter} className="text-gray-300 hover:text-white w-6 h-6" />
               </Link>
               <Link href="#">
-              <FontAwesomeIcon icon={faFacebookF} className="text-gray-300 hover:text-white w-6 h-6" />
+                <FontAwesomeIcon icon={faFacebookF} className="text-gray-300 hover:text-white w-6 h-6" />
               </Link>
               <Link href="https://wa.me/+251940677928" target="blank">
-              <FontAwesomeIcon icon={faWhatsapp} className="text-gray-300 hover:text-white w-6 h-6" />
+                <FontAwesomeIcon icon={faWhatsapp} className="text-gray-300 hover:text-white w-6 h-6" />
               </Link>
               <Link href="https://t.me/AradomL" target='blank'>
-              <FontAwesomeIcon icon={faTelegram} className="text-gray-300 hover:text-white w-6 h-6" />
+                <FontAwesomeIcon icon={faTelegram} className="text-gray-300 hover:text-white w-6 h-6" />
               </Link>
             </div>
             
             <div className="flex flex-col sm:flex-row justify-center md:justify-start space-y-4 sm:space-y-0 sm:space-x-4">
-              <Link href="/Aradom_Leake_CV.pdf" className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-full transition duration-300">
+              {/* Update the Link to point to the CV file in the public folder */}
+              <Link href="/Aradom-Leake-CV.pdf" download className="bg-blue-500 hover:bg-blue-600 text-white px-6 py-3 rounded-full transition duration-300">
                 Download CV
               </Link>
               <Link href="#contact" className="bg-transparent hover:bg-white hover:text-gray-800 text-white px-6 py-3 rounded-full border-2 border-white transition duration-300">
